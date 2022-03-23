cbcc61af47bd01930f53b4a6e7cb704dec6a6501
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-03-24 00:03:42 +0100

    updated attribute directive

--- src/app/transform.directive.ts
+++ src/app/transform.directive.ts
@@ -1,15 +1,28 @@
-import { Directive, Input, OnInit } from '@angular/core';
+import { Directive, OnInit, Input, Renderer2, ElementRef } from '@angular/core';
+import { JsonNode } from './main-text/jsonNode.model';
 
 @Directive({
   selector: '[appTransform]'
 })
 export class TransformDirective implements OnInit {
   @Input() rule: any;
+  @Input() data?: JsonNode;
 
-  constructor() { }
+  findAttributeValue(attributes: [{ name: string, value: string }] | null | undefined, attribute: string) {
+    const val = attributes?.find(e => e.name === attribute);
+    return val?.value;
+  }
+
+  constructor(private renderer: Renderer2, private elRef: ElementRef) { }
 
   ngOnInit() {
-    console.log(this.rule);
+    if (this.rule) {
+      if (this.rule.attributes_transform) {
+        this.rule.attributes_transform.forEach((attr: any) => {
+          return this.renderer.setAttribute(this.elRef.nativeElement, attr.target, this.findAttributeValue(this.data?.attributes, attr.start) || 'no-attr-found');
+        });
+      }
+    }
   }
 
 }
