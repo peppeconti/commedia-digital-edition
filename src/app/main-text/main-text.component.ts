874daed0a3da5e4e9e19ea42138c48350a4aa311
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-03-23 22:31:04 +0100

    added comparing attribute

--- src/app/main-text/main-text.component.ts
+++ src/app/main-text/main-text.component.ts
@@ -15,8 +15,16 @@ export class MainTextComponent implements OnInit {
 
   }
 
+  compare(arg_1: any, arg_2: any) {
+    return arg_1.map((a: any) => {
+        return arg_2.some((b: any) => {
+            return a.name === b.name && a.value === b.value
+        })
+    });
+}
+
   findRule(data: any) {
-    return this.rules.find((e: any) => data?.tagName === e.element);
+    return this.rules.find((e: any) => (data?.tagName === e.element && this.compare(e.select_attr, data?.attributes).every((e:boolean) => e === true)));
   }
 
   ngOnInit(): void {
