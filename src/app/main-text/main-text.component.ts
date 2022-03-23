759b11571f276cd5ff8488fea379c18de9db1762
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-03-23 22:01:03 +0100

    update

--- src/app/main-text/main-text.component.ts
+++ src/app/main-text/main-text.component.ts
@@ -6,11 +6,19 @@ import { JsonNode } from './jsonNode.model';
   templateUrl: './main-text.component.html',
   styleUrls: ['./main-text.component.css']
 })
-export class MainTextComponent {
+export class MainTextComponent implements OnInit {
   @Input() main_text: JsonNode | undefined;
   @Input() rules: any;
+  @Input() rule: any;
 
   constructor() {
+
+  }
+
+  findRule(data: any) {
+    return this.rules.find((e: any) => data?.tagName === e.element);
+  }
+
+  ngOnInit(): void {
   }
 }
-
