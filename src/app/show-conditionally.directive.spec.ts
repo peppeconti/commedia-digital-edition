168fcb07de3755ef1cec0362f9e0067ba25a8674
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-03-30 14:40:55 +0200

    delete file

--- src/app/show-conditionally.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowConditionallyDirective } from './show-conditionally.directive';
-
-describe('ShowConditionallyDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowConditionallyDirective();
-    expect(directive).toBeTruthy();
-  });
-});
