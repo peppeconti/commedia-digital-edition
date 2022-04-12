c3d227b14804518cb081d3f4386978c5f7f88a2e
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-04-12 11:13:09 +0200

    added info icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBookOpen, faBook } from '@fortawesome/free-solid-svg-icons';
+import { faBookOpen, faBook, faInfo } from '@fortawesome/free-solid-svg-icons';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ServiceSettings } from './shared/settings.service';
 import { ServiceFetch } from './shared/fetch.service';
@@ -41,6 +41,6 @@ import { NoteTextComponent } from './note-container/note-text/note-text.componen
 })
 export class AppModule { 
   constructor(library: FaIconLibrary) {
-    library.addIcons(faBook, faBookOpen);
+    library.addIcons(faBook, faBookOpen, faInfo);
   }
 }
