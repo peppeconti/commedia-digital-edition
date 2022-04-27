cab296293476ed2431c7fe819d83e74b33fbb1f0
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-04-27 20:51:33 +0200

    deleted unused fonts

--- src/app/main-text/main-text.component.ts
+++ src/app/main-text/main-text.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { JsonNode } from '../shared/jsonNode.model';
 
 @Component({
   selector: 'app-main-text',
